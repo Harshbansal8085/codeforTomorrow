8137c0ad9707194b61ee03b5a8f4915b23359337
Harsh_8085 <harshbansal8085@gmail.com>
2026-03-06 12:56:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from "react";
+import img from "./image.png";
+import "./App.css";
+const intialposts= [
+  {
+    id:1,
+    title:"sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body:"quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    image:img,
+  },
+  
+  {
+    id:2,
+    title:"qui est esse",
+    body:"est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+    image:img,
+  },
+  {
+    id:3,
+    title:"ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    body:"et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    image:img,
+  }
+];
 function App() {
+  const [posts, setPosts] = useState(intialposts);
+
+  const handleDelete=(id)=>{
+    setPosts((prev) => prev.filter((posts) => posts.id !== id));  
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Posts</h1>
+    <div className="Posts-grid">
+      {posts.map((posts) => (
+        <div key={posts.id} className="post-card">
+        <button 
+        className="close-button"
+        onClick={() => handleDelete(posts.id)}>Delete</button>
+       <img src={posts.image} alt={posts.title} className="post-image"/>
+        <h2 className="posts-title">{posts.title}</h2>
+        <p className="posts-body">{posts.body}</p>
+        </div>
+      ))}
+    </div>
     </div>
   );
-}
-
-export default App;
+  }
+  
+  export default App;
